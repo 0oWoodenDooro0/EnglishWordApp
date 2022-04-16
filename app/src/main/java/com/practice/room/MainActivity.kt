7db3df83b91a80cb7a2eec1a3ccd7d2356c395b6
WoodenDoor <vincent031525@gmail.com>
2022-04-16 12:19:48 +0800

    add new item in toolbar

--- app/src/main/java/com/practice/room/MainActivity.kt
+++ app/src/main/java/com/practice/room/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.practice.room
 
 import android.os.Bundle
-import android.util.Log
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.practice.room.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -25,7 +23,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.wordsToolbar)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_nav_controller) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(binding.fragmentNavController.id) as NavHostFragment
         val navController = navHostFragment.findNavController()
 //        Log.d("MainActivity", navController.toString())
         appBarConfiguration = AppBarConfiguration(navController.graph)
@@ -34,8 +33,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_nav_controller) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_nav_controller) as NavHostFragment
         val navController = navHostFragment.findNavController()
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu);
+        return true
     }
 }
